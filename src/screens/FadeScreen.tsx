d12b86bbd8250145c813392337cb0dd50243537b
Daniel Romero <tutorialesdanielromero@gmail.com>
2022-12-04 17:46:10 -0500

    animacion de fadein y fadeout

--- src/screens/FadeScreen.tsx
+++ src/screens/FadeScreen.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Animated, Button } from 'react-native';
+import { useFade } from '../hooks/useFade';
 
 export const FadeScreen = () => {
+  const { opacity, fadeIn, fadeOut } = useFade();
+
   return (
-    <View
+    <Animated.View
       style={{
         flex: 1,
-        backgroundColor: 'red',
+        backgroundColor: 'grey',
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <View style={{ backgroundColor: '#084f61', width: 150, height: 150 }} />
-    </View>
+      <Animated.View
+        style={{
+          backgroundColor: '#084f61',
+          width: 150,
+          height: 150,
+          borderColor: 'white',
+          borderWidth: 10,
+          marginBottom: 10,
+          opacity: opacity,
+        }}
+      />
+      <Button title="FadeIn" onPress={fadeIn} />
+      <Button title="FadeOut" onPress={fadeOut} />
+    </Animated.View>
   );
 };
